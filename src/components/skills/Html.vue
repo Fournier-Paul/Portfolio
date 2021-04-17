ce79e787760a8639b7f0f80c743555bc62ecd205
PaulFournier <44707149+Paul1465@users.noreply.github.com>
2021-04-17 12:50:57 +0200

    docker in progress

--- src/components/skills/Html.vue
+++ src/components/skills/Html.vue
@@ -36,8 +36,17 @@
             <img src="img/seo.jpg" alt="Lights" class="mt-4" style="height:150px">
             </div>
         </div>
+        <div class="col-md-3 col-sm-6 mt-5 mb-5">
+            <div class="thumbnail" data-aos="fade-left" data-aos-duration="1000">
+            <img src="img/docker.png" alt="Lights" class="mt-4" style="height:150px">
+            <br>
+                <div class="spinner-border text-info mb-3" role="status">
+                    <span class="sr-only">Loading...</span>
+                </div>
+                <p>En cours</p>
+            </div>
+        </div>
 </div>
-
 </template>
 
 
