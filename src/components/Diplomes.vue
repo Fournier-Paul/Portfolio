340ab1b82ef6312ece3daab066ad5c9eda725660
Fournier-Paul <paul.fournier.3314@gmail.com>
2025-04-17 20:26:34 +0200

    add padding

--- src/components/Diplomes.vue
+++ src/components/Diplomes.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="container-fluid fixed-top-position">
-      <div class="row px-3"> <!-- padding horizontal uniforme -->
+    <div class="container-fluid fixed-top-position pt-150">
+      <div class="row px-3">
         
         <div class="col-md-6 col-xs-12 mt-4">
           <div class="card h-100">
